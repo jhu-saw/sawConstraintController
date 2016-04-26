ebeab8fd8a78bf1c8acc7b59fe0a76d0146a1645
Anton Deguet <anton.deguet@jhu.edu>
2016-04-26 15:03:12 -0400

    Re-organized files to clearly separate components from examples.  In catkin, now use two different package.xml

--- examples/SimpleCommunication/prmSimpleRobotKinematicsState.h
+++ examples/SimpleCommunication/prmSimpleRobotKinematicsState.h
@@ -1,7 +1,3 @@
-#ifndef _prmSimpleRobotKinematicsState_h
-#define _prmSimpleRobotKinematicsState_h
-
-
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-    */
 /* ex: set filetype=cpp softtabstop=4 shiftwidth=4 tabstop=4 cindent expandtab: */
 
@@ -21,6 +17,9 @@
  
  --- end cisst license ---
  */
+
+#ifndef _prmSimpleRobotKinematicsState_h
+#define _prmSimpleRobotKinematicsState_h
 
 #include <sawConstraintController/prmKinematicsState.h>
 
