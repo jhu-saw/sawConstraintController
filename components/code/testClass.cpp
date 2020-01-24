d0d421f194686471b5b4844cb62718fa4ecf5193
Anton Deguet <anton.deguet@jhu.edu>
2020-01-24 14:06:29 -0500

    removed debug files

--- components/code/testClass.cpp
+++
@@ -1,9 +0,0 @@
-#include <sawConstraintController/testClass.h>
-
-testClass::testClass() : mtsVFCartesianTranslation()
-{
-
-}
-
-testClass::testClass(const std::string &name, mtsVFDataBase *data) : mtsVFCartesianTranslation(name,data) {
-}
